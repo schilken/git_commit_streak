087413b90d0728dac8b3fbbd7cdb0d8d2bad221c
Alfred Schilken <alfred@schilken.de>
2023-02-18 17:46:23 +0100

    make SystemCommand a dependency for GitInfoRepository, prepare for tests

---
+++ lib/utils/system_command.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mixin_logger/mixin_logger.dart' as log;
+
+class SystemCommand {
+  Future<List<String>> runShellCommand(
+    executable,
+    arguments,
+    String directory,
+  ) async {
+    final process = await Process.run(
+      executable,
+      arguments,
+      workingDirectory: directory,
+    );
+    if (process.exitCode != 0) {
+      log.w('stderr: ${process.stderr}');
+      return [];
+    } else {
+      final lines = process.stdout.split('\n');
+      return lines;
+    }
+  }
+}
+
+final systemCommandProvider = Provider<SystemCommand>((ref) => SystemCommand());
