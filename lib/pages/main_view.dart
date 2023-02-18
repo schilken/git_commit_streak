e20df0710f6cb9778c2218a892370749b899b484
Alfred Schilken <alfred@schilken.de>
2023-02-18 17:55:13 +0100

    start scan on startUp

--- lib/pages/main_view.dart
+++ lib/pages/main_view.dart
@@ -4,6 +4,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:macos_ui/macos_ui.dart';
 
 import '../providers/app_notifier.dart';
+import '../providers/git_info_notifier.dart';
 import 'help_page.dart';
 import 'home_page.dart';
 import 'settings_page.dart';
@@ -17,6 +18,15 @@ class MainView extends ConsumerStatefulWidget {
 
 class _MainViewState extends ConsumerState<MainView> {
   int _pageIndex = 0;
+
+  @override
+  void initState() {
+    Future<void>.delayed(const Duration(milliseconds: 100), () {
+      final currentDirectory = ref.watch(appNotifier).currentDirectory;
+      ref.read(gitInfoNotifier.notifier).scan(currentDirectory);
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
