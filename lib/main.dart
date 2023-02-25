e469eca01922fb226b1ad2dd7b240297f78943f2
Alfred Schilken <alfred@schilken.de>
2023-02-25 12:00:36 +0100

    remove handling of check on startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:macos_ui/macos_ui.dart';
-import 'package:package_info_plus/package_info_plus.dart';
 import 'package:pubspec_parse/pubspec_parse.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:mixin_logger/mixin_logger.dart' as log;
@@ -20,29 +17,13 @@ Future<void> main() async {
   final pubspec = Pubspec.parse(await rootBundle.loadString('pubspec.yaml'));
   final version = pubspec.version;
   await log.initLogger(loggerFolder);
-  final isCheckCommitStreak =
-      Platform.environment['CHECK_COMMIT_STREAK'] ?? 'false';
   log.i(
-      'version from pubspec.yaml: $version, isCheckCommitStreak: $isCheckCommitStreak');
-
+      'version from pubspec.yaml: $version');
   sharedPreferences.setString('appVersion', version.toString());
-  final container = ProviderContainer(
+  runApp(ProviderScope(
     overrides: [
       sharedPreferencesProvider.overrideWithValue(sharedPreferences),
     ],
-//    observers: [AsyncErrorLogger()],
-  );
-  if (isCheckCommitStreak == 'true') {
-    await container.read(reminderProvider).checkCommitsOfToday();
-    await Future<void>.delayed(const Duration(milliseconds: 1000));
-    log.i('before SystemNavigator.pop');
-    SystemChannels.platform.invokeMethod('SystemNavigator.pop');
-  }
-  PackageInfo packageInfo = await PackageInfo.fromPlatform();
-  log.i(
-      'bundleID: ${packageInfo.packageName} executable: ${Platform.resolvedExecutable}');
-  runApp(UncontrolledProviderScope(
-    container: container,
     child: const MainApp(),
   ));
 }
@@ -53,7 +34,6 @@ class MainApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     return MacosApp(
       title: 'GitCommitStreak',
       theme: MacosThemeData.light(),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,12 +7,10 @@ import Foundation
 
 import flutter_local_notifications
 import macos_ui
-import package_info_plus
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   MacOSUiPlugin.register(with: registry.registrar(forPlugin: "MacOSUiPlugin"))
-  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
